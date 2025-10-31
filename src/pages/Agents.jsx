d7433d1e4f1181bbfa1cad1a25fb9c383a5b0f3b
Jeevesh Mahajan <jeeveshmahajan00@gmail.com>
2025-10-31 11:23:30 +0530

    feat: dynamically render sales agent list

--- src/pages/Agents.jsx
+++ src/pages/Agents.jsx
@@ -1,18 +1,28 @@
 import AgentForm from "../components/AgentForm";
 import useAnvayaContext from "../context/AnvayaContext";
+import useFetch from "../hooks/useFetch";
 
 const Agents = () => {
-  const { openFormModal } = useAnvayaContext();
+  const { openFormModal, API } = useAnvayaContext();
+  const { data, loading, error } = useFetch(`${API}/agents`);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>An error occured!</p>;
+
+  const salesAgentListing = data?.agents?.map((agent, index) => (
+    <li key={index}>
+      <p>
+        Agent: {agent.name} - {agent.email}
+      </p>
+    </li>
+  ));
 
   return (
     <>
       <h1>Sales Agent Management</h1>
       <h2>Sales Agent List</h2>
 
-      <ul className="lead-list">
-        <li>Agent: [John Doe] - [Email]</li>
-        <li>Agent: [Jane Smith] - [Email]</li>
-      </ul>
+      <ul className="lead-list">{salesAgentListing}</ul>
 
       <button onClick={openFormModal} className="btn btn-primary">
         Add New Agent
