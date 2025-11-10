e2fbca497a9a4239dd6ff5af9c36ad6bd2a02dcf
Jeevesh Mahajan <jeeveshmahajan00@gmail.com>
2025-11-10 17:14:47 +0530

    feat: add, render comments and manage styling

--- src/pages/LeadDetails.jsx
+++ src/pages/LeadDetails.jsx
@@ -1,12 +1,61 @@
 import { useParams } from "react-router-dom";
+import Select from "react-select";
 
 import useAnvayaContext from "../context/AnvayaContext";
+import useFetch from "../hooks/useFetch";
+import { useState } from "react";
 
 const LeadDetails = () => {
   const { leadId } = useParams();
-  const { leadsList } = useAnvayaContext();
+  const { leadsList, API, salesAgentsList } = useAnvayaContext();
+
+  const [commentText, setCommentText] = useState("");
+  const [commentAuthor, setCommentAuthor] = useState(null);
+  const authorOptions = salesAgentsList?.map((agent) => ({
+    value: `${agent._id}`,
+    label: `${agent.name}`,
+  }));
 
   const selectedLead = leadsList?.find((lead) => lead._id === leadId);
+  const { data, loading, error } = useFetch(`${API}/leads/${leadId}/comments`);
+
+  const commentsListing = data?.comments.map((comment) => (
+    <li className="lead-list-item comment" key={comment._id}>
+      <strong>{comment.author?.name}</strong> -{" "}
+      {new Date(comment.createdAt).toLocaleString()} <br />
+      Comment: {comment.commentText}
+    </li>
+  ));
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+
+    const addComment = async () => {
+      try {
+        const res = await fetch(`${API}/leads/${leadId}/comments`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            commentText: `${commentText}`,
+            author: `${commentAuthor.value}`,
+          }),
+        });
+
+        if (!res.ok) {
+          throw new Error("❌ Error adding the comment.");
+        }
+
+        const data = await res.json();
+        console.log("✅ Comment added successfully:", data);
+      } catch (error) {
+        throw new Error(`❌ Failed to add comment: ${error.message}`);
+      }
+    };
+
+    addComment();
+  };
 
   return (
     <>
@@ -39,7 +88,47 @@ const LeadDetails = () => {
         <p>Loading Lead Details...</p>
       )}
 
-      <h2>Comments</h2>
+      <div className="comment-section">
+        <h2>Comments</h2>
+        {loading ? (
+          <p>Loading Comments...</p>
+        ) : data?.comments?.length > 0 ? (
+          <ul className="lead-list">{commentsListing}</ul>
+        ) : (
+          <p>No Comments yet.</p>
+        )}
+      </div>
+
+      <h2 className="add-comment-header">Add a Comment:</h2>
+      <form onSubmit={handleCommentSubmit}>
+        <div>
+          <label className="form-label" htmlFor="commentText">
+            Comment:
+          </label>
+          <input
+            id="commentText"
+            type="text"
+            placeholder="Type comment here..."
+            className="form-control"
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+          />
+        </div>
+        <div>
+          <label className="form-label" htmlFor="author">
+            Author:
+          </label>
+          <Select
+            id="author"
+            options={authorOptions}
+            placeholder="Select your name.."
+            className="form-select-like"
+            value={commentAuthor}
+            onChange={(e) => setCommentAuthor(e)} // in case of Select, the e itself is the value
+          />
+        </div>
+        <button className="btn btn-primary form-submit">Submit</button>
+      </form>
     </>
   );
 };
