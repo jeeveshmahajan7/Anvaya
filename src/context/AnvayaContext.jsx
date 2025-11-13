00355fbf806bc2af4985b43fb2e9199f3e0cc1ca
Jeevesh Mahajan <jeeveshmahajan00@gmail.com>
2025-11-13 07:41:49 +0530

    feat: add functionality to auto refresh agents and leads after new addition

--- src/context/AnvayaContext.jsx
+++ src/context/AnvayaContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 const AnvayaContext = createContext();
 const useAnvayaContext = () => useContext(AnvayaContext);
@@ -11,66 +12,49 @@ export const AnvayaProvider = ({ children }) => {
   const [isLeadModalOpen, setIsLeadModalOpen] = useState(false);
 
   const [salesAgentsList, setSalesAgentsList] = useState([]);
-  const [loadingAgents, setLoadingAgents] = useState(false);
-  const [errorAgents, setErrorAgents] = useState(null);
+  const [refreshAgents, setRefreshAgents] = useState(false); // state to manage auto refresh of agents
 
   const [leadsList, setLeadsList] = useState([]);
-  const [loadingLeads, setLoadingLeads] = useState(false);
-  const [errorLeads, setErrorLeads] = useState(null);
+  const [refreshLeads, setRefreshLeads] = useState(false); // state to manage auto refresh of leads
 
   const openFormModal = () => setIsFormModalOpen(true);
   const closeFormModal = () => setIsFormModalOpen(false);
   const openLeadModal = () => setIsLeadModalOpen(true);
   const closeLeadModal = () => setIsLeadModalOpen(false);
 
+  // logic to auto refresh agents
+  const onAgentAdded = () => {
+    setRefreshAgents((prev) => !prev);
+  };
+
+  const {
+    data: agentsData,
+    loading: loadingAgents,
+    error: errorAgents,
+  } = useFetch(`${API}/agents`, [refreshAgents]); // useFetch fetches new data when refreshAgents dependency is set to true
+
   useEffect(() => {
-    const fetchAgents = async () => {
-      try {
-        setLoadingAgents(true);
-        setErrorAgents(null);
-        const res = await fetch(`${API}/agents`);
+    if (agentsData?.agents) {
+      setSalesAgentsList(agentsData.agents);
+    }
+  }, [agentsData]); // updates when new Agent data is fetched
 
-        if (!res.ok) {
-          throw new Error("Failed to fetch agents");
-        }
+  // logic to auto refresh leads
+  const onLeadAdded = () => {
+    setRefreshLeads((prev) => !prev);
+  };
 
-        const data = await res.json();
+  const {
+    data: leadsData,
+    loading: loadingLeads,
+    error: errorLeads,
+  } = useFetch(`${API}/leads`, [refreshLeads]); // updates when new Leads data is fetched
 
-        if (data?.agents) {
-          setSalesAgentsList(data.agents);
-        }
-      } catch (error) {
-        throw new Error("Error fetching agents:", error);
-      } finally {
-        setLoadingAgents(false);
-      }
-    };
-
-    const fetchLeads = async () => {
-      try {
-        setLoadingLeads(true);
-        setErrorLeads(null);
-        const res = await fetch(`${API}/leads`);
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch leads.");
-        }
-
-        const data = await res.json();
-
-        if (data?.leads) {
-          setLeadsList(data.leads);
-        }
-      } catch (error) {
-        throw new Error("Error fetching leads:", error);
-      } finally {
-        setLoadingLeads(false);
-      }
-    };
-
-    fetchLeads();
-    fetchAgents();
-  }, []);
+  useEffect(() => {
+    if (leadsData?.leads) {
+      setLeadsList(leadsData.leads);
+    }
+  }, [leadsData]);
 
   return (
     <AnvayaContext.Provider
@@ -84,6 +68,8 @@ export const AnvayaProvider = ({ children }) => {
         leadsList,
         loadingLeads,
         errorLeads,
+        onAgentAdded,
+        onLeadAdded,
         setSalesAgentsList,
         setIsFormModalOpen,
         setIsLeadModalOpen,
